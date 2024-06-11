08bdd21af843fec613fab2687f8d1f292211b88a
ddolboghi <rhdfyd128@gmail.com>
2024-06-11 19:30:09 +0900

    fix: jwt 토큰 검증 필터 원복

--- src/main/java/com/skyhorsemanpower/gatewayserver/filter/AuthorizationHeaderFilter.java
+++ src/main/java/com/skyhorsemanpower/gatewayserver/filter/AuthorizationHeaderFilter.java
@@ -1,72 +1,66 @@
 package com.skyhorsemanpower.gatewayserver.filter;
 
-import com.skyhorsemanpower.gatewayserver.exception.CustomException;
-import com.skyhorsemanpower.gatewayserver.exception.ResponseStatus;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
+import com.skyhorsemanpower.gatewayserver.security.JwtTokenProvider;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
-@Slf4j
 @Component
+@Slf4j
 public class AuthorizationHeaderFilter extends
     AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
 
-    private final SecretKey key;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public AuthorizationHeaderFilter(@Value("${JWT.SECRET_KEY}") String key) {
+    @Autowired
+    public AuthorizationHeaderFilter(JwtTokenProvider jwtTokenProvider) {
         super(Config.class);
-        byte[] keyBytes = Decoders.BASE64.decode(key);
-        this.key = new SecretKeySpec(keyBytes, SignatureAlgorithm.HS256.getJcaName());
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    static class Config {
+
     }
 
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            ServerHttpRequest req = exchange.getRequest();
-            HttpHeaders header = req.getHeaders();
+            ServerHttpRequest request = exchange.getRequest();
 
-            if (!header.containsKey(HttpHeaders.AUTHORIZATION)) {
-                throw new CustomException(ResponseStatus.NOT_FOUND_TOKEN);
+            HttpHeaders headers = request.getHeaders();
+            if (!headers.containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "No Authorization header", HttpStatus.UNAUTHORIZED);
             }
 
-            String authorization = header.get(HttpHeaders.AUTHORIZATION).get(0);
-            if (authorization == null || !authorization.startsWith("Bearer")) {
-                throw new CustomException(ResponseStatus.INVALID_TOKEN);
-            }
+            String authorizationHeader = headers.get(HttpHeaders.AUTHORIZATION).get(0);
 
-            String jwt = authorization.substring(7);
-            isValid(jwt);
-            return chain.filter(exchange);
+            String jwt = authorizationHeader.replace("Bearer ", "");
+
+            jwtTokenProvider.validateJwtToken(jwt);
+
+            ServerHttpRequest newRequest = request.mutate()
+                .header("X-AUTH-TOKEN", jwt)
+                .build();
+
+            return chain.filter(exchange.mutate().request(newRequest).build());
         };
     }
 
-    private void isValid(String jwt) {
+    private Mono<Void> onError(ServerWebExchange exchange, String errorMessage,
+        HttpStatus httpStatus) {
+        log.error(errorMessage);
 
-        try {
-            Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(jwt)
-                .getBody()
-                .getSubject();
-        } catch (ExpiredJwtException e) {
-            throw new CustomException(ResponseStatus.EXPIRED_TOKEN);
-        } catch (Exception e) {
-            throw new CustomException(ResponseStatus.INVALID_TOKEN);
-        }
-    }
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
 
-    public static class Config {
-
+        return response.setComplete();
     }
 }
