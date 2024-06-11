b694f14c65d2be89d0cbc6c1cf5ffbc089d05a07
ddolboghi <rhdfyd128@gmail.com>
2024-06-11 16:24:36 +0900

    fix: 주석 제거

--- src/main/java/com/skyhorsemanpower/gatewayserver/security/JwtTokenProvider.java
+++ src/main/java/com/skyhorsemanpower/gatewayserver/security/JwtTokenProvider.java
@@ -11,6 +11,7 @@ import io.jsonwebtoken.security.SignatureException;
 import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
@@ -29,7 +30,6 @@ public class JwtTokenProvider {
 
     private Claims getClaimsFromJwtToken(String token) {
         try {
-//            this.SECRET = env.getProperty("JWT.SECRET_KEY");
             return Jwts.parserBuilder()
                 .setSigningKey(SECRET.getBytes())
                 .build()
